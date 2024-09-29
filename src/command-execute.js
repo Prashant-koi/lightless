e4b89479ad6065600e45ec3ac3e162075a854227
TheGrandPacific <lordofvoid100@gmail.com>
2024-09-29 14:23:20 -0500

    Completed a slash command
    Too many changes to comment

--- src/command-execute.js
+++ src/command-execute.js
@@ -12,6 +12,7 @@ client.on('interactionCreate', async interaction => {
       await interaction.reply(`${interaction.guild.name} is the name of your guild.`);
     }
   
+    //For command /serverinfo
     if (interaction.commandName === 'serverinfo') {
       const channelName = interaction.channel;
       const guild = interaction.guild;
@@ -20,7 +21,7 @@ client.on('interactionCreate', async interaction => {
         console.log('This command only works in a guild.');
         return;
       }
-  
+      
       // Fetching the members of the guild where the command was used
       await guild.members.fetch();
   
@@ -35,17 +36,26 @@ client.on('interactionCreate', async interaction => {
           membersWithAdmin++;
         }
       });
+      creationDate = Math.floor((interaction.guild.createdTimestamp)/1000);
+
+      const textChannels = interaction.guild.channels.cache.filter(channel => channel.type === 'GUILD_TEXT');
   
       const infoEmbed = new EmbedBuilder() 
         .setAuthor({name: 'Server info', iconURL:`${interaction.guild.iconURL()}`})
-        .setColor("#ffffff")
+        .setColor("#ff0145")
         .setThumbnail(`${interaction.guild.iconURL()}`)
         .setFooter({text : `${interaction.user.username}`})
-        .setDescription(`Server Name: ${interaction.guild.name} \n
-          Server owner: ${owner.user.tag}\n
-          Number of Members: ${interaction.guild.memberCount}\n
-          Number of Banned Members: ${guild.bans.cache.size}\n
-          Number of Admins: ${membersWithAdmin}\n
+        .setDescription(`
+- **Server Name:** ${interaction.guild.name}
+- **Server Owner:** ${owner.user.tag}
+- **Creation date:** <t:${creationDate}:F>
+- **Number of Members:** ${interaction.guild.memberCount}
+- **Server Icon:** [Click Here](${interaction.guild.iconURL()})
+- **Server Boosts:** ${interaction.guild.premiumSubscriptionCount}
+- **Roles:** ${((await interaction.guild.roles.fetch()).size)}
+- **Channels:** ${(await interaction.guild.channels.fetch()).size}
+- **Number of Banned Members:** ${guild.bans.cache.size}
+- **Number of Admins:** ${membersWithAdmin}
   
         `);
         
--- src/index.js
+++ src/index.js
@@ -17,55 +17,6 @@ client.on('ready', () => {
   require('./command-execute');
 });
 
-// client.on('interactionCreate', async interaction => {
-//   if (!interaction.isChatInputCommand()) return;
-
-//   if (interaction.commandName === 'hello') {
-//     await interaction.reply(`${interaction.guild.name} is the name of your guild.`);
-//   }
-
-//   if (interaction.commandName === 'serverinfo') {
-//     const channelName = interaction.channel;
-//     const guild = interaction.guild;
-
-//     if (!guild) {
-//       console.log('This command only works in a guild.');
-//       return;
-//     }
-
-//     // Fetching the members of the guild where the command was used
-//     await guild.members.fetch();
-
-//     const owner = await interaction.guild.fetchOwner();
-
-//     const permissionForAdmin = PermissionsBitField.Flags.Administrator;
-//     let membersWithAdmin = 0;
-
-//     // Counting members with Administrator permission excluding bots
-//     guild.members.cache.forEach(member => {
-//       if (member.permissions.has(permissionForAdmin) && !member.user.bot) {
-//         membersWithAdmin++;
-//       }
-//     });
-
-//     const infoEmbed = new EmbedBuilder() 
-//       .setAuthor({name: 'Server info', iconURL:`${interaction.guild.iconURL()}`})
-//       .setColor("#ffffff")
-//       .setThumbnail(`${interaction.guild.iconURL()}`)
-//       .setFooter({text : `${interaction.user.username}`})
-//       .setDescription(`Server Name: ${interaction.guild.name} \n
-//         Server owner: ${owner.user.tag}\n
-//         Number of Members: ${interaction.guild.memberCount}\n
-//         Number of Banned Members: ${guild.bans.cache.size}\n
-//         Number of Admins: ${membersWithAdmin}\n
-
-//       `);
-      
-//       await (channelName.send({ embeds: [infoEmbed] }));
-    
-//   }
-// });
-
 // Exporting the client here so that I can import it in other files and not create multiple clients
 module.exports = client;
 
